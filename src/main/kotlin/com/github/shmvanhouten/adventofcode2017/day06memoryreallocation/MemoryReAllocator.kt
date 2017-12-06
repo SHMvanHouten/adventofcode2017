3512e6755e8562315bcbdddcf667e1172a07a69b
SvanHouten <svanhouten@gmail.com>
2017-12-06 14:02:37 +0100

    solved part 2 of day 6

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day06memoryreallocation/MemoryReAllocator.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day06memoryreallocation/MemoryReAllocator.kt
@@ -2,20 +2,39 @@ package com.github.shmvanhouten.adventofcode2017.day06memoryreallocation
 
 class MemoryReAllocator(val floorBuilder: FloorBuilder = FloorBuilder()) {
 
-    fun reallocateMemoryUntilDuplicateSituationOccurs(rawInput: String): Int {
+    fun reallocateMemoryUntilDuplicateSituationOccurs(rawInput: String): Pair<Int, FloorOfMemoryBanks> {
         var currentFloor = floorBuilder.buildFloorFromRawInput(rawInput)
         var floorStates = setOf(currentFloor.getCurrentStateOfFloors())
 
         var steps = 0
-        while (true){
+        while (true) {
             steps++
 
             currentFloor = currentFloor.reallocateBiggestBank()
             val currentStateOfFloors = currentFloor.getCurrentStateOfFloors()
-            if(floorStates.any{ it == currentStateOfFloors }){
-                return steps
+            if (floorStates.any { it == currentStateOfFloors }) {
+                return Pair(steps, currentFloor)
             }
             floorStates = floorStates.plus(currentStateOfFloors)
+        }
+    }
+
+    fun reallocateMemoryUntilDuplicateSituationOccursASecondTimeAfterTheFirst(rawInput: String): Int {
+        val (_, floor) = reallocateMemoryUntilDuplicateSituationOccurs(rawInput)
+        val floorStateToCompareAgainst = floor.getCurrentStateOfFloors()
+
+        var currentFloor = floor
+
+        var steps = 0
+        while (true) {
+            steps++
+
+            currentFloor = currentFloor.reallocateBiggestBank()
+            val currentStateOfFloors = currentFloor.getCurrentStateOfFloors()
+
+            if(currentStateOfFloors == floorStateToCompareAgainst){
+                return steps
+            }
         }
     }
 }
--- src/test/kotlin/com/github/shmvanhouten/adventofcode2017/day06memoryreallocation/MemoryReAllocatorTest.kt
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode2017/day06memoryreallocation/MemoryReAllocatorTest.kt
@@ -10,15 +10,30 @@ class MemoryReAllocatorTest {
     fun `it should find how many re-allocations happen before it finds a duplicate state`() {
         val reAllocator = MemoryReAllocator()
         val rawInput = "0\t2\t7\t0"
-        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccurs(rawInput), equalTo(5))
+        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccurs(rawInput).first, equalTo(5))
+    }
+
+    @Test
+    fun `it should find how many re-allocations happen before it finds a duplicate state twice`() {
+        val reAllocator = MemoryReAllocator()
+        val rawInput = "0\t2\t7\t0"
+        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccursASecondTimeAfterTheFirst(rawInput), equalTo(4))
     }
 
     @Test
     fun `it should solve the challenge input`() {
         val reAllocator = MemoryReAllocator()
 
-        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccurs(day06ChallengeInput), equalTo(4074))
+        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccurs(day06ChallengeInput).first, equalTo(4074))
     }
+
+    @Test
+    fun `it should find how many re-allocations happen before it finds a duplicate state twice for the challenge input`() {
+        val reAllocator = MemoryReAllocator()
+
+        assertThat(reAllocator.reallocateMemoryUntilDuplicateSituationOccursASecondTimeAfterTheFirst(day06ChallengeInput), equalTo(4))
+    }
+
 }
 
 val day06ChallengeInput = "11\t11\t13\t7\t0\t15\t5\t5\t4\t4\t1\t1\t7\t1\t15\t11"
