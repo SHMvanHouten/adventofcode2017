693018d8909001e789e38748cc673fb016e7491f
SvanHouten <svanhouten@gmail.com>
2017-12-08 13:13:40 +0100

    BSR: clean up and renamed some methods

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day07recursivecircus/TowerBalanceRestorer.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day07recursivecircus/TowerBalanceRestorer.kt
@@ -4,34 +4,40 @@ class TowerBalanceRestorer {
 
     fun findCorrectWeightForUnbalancedProgram(towerSupportPrograms: List<TowerSupportProgram>): Int {
 
-        val programWithUnevenlyBalancedChildren = findProgramWithUnevenBalance(towerSupportPrograms)
+        val programWithUnevenlyBalancedChildren = findProgramWithUnevenlyBalancedChildren(towerSupportPrograms)
 
-        return findWhatWeightShouldBeForTheDivergentChild(programWithUnevenlyBalancedChildren.towersItSupports)
+        return findWhatDivergentChildWeightShouldBe(programWithUnevenlyBalancedChildren.towersItSupports)
     }
 
-    private fun findWhatWeightShouldBeForTheDivergentChild(unbalancedChildren: List<TowerSupportProgram>): Int {
-        val unbalancedAndBalacedSupportPrograms = unbalancedChildren.
-                distinctBy { it.totalWeight }
-                .groupBy { childToCompare -> unbalancedChildren.count { it.totalWeight == childToCompare.totalWeight } == 1 }
-        val totalWeightToAchieve = unbalancedAndBalacedSupportPrograms.getValue(false).first().totalWeight
-        val divergentChild = unbalancedAndBalacedSupportPrograms.getValue(true).first()
+    private fun findWhatDivergentChildWeightShouldBe(unbalancedChildren: List<TowerSupportProgram>): Int {
+
+        val unbalancedAndBalancedSupportPrograms = unbalancedChildren
+                .groupBy { childToCompare ->
+                    unbalancedChildren.count { it.totalWeight == childToCompare.totalWeight } == 1
+                }
+
+        val divergentChild = unbalancedAndBalancedSupportPrograms
+                .getValue(true).first()
         val currentTotalWeight = divergentChild.totalWeight
 
-        return (totalWeightToAchieve - currentTotalWeight) + divergentChild.weight
+        val totalWeightToAchieve = unbalancedAndBalancedSupportPrograms
+                .getValue(false)
+                .first().totalWeight
 
+        return (totalWeightToAchieve - currentTotalWeight) + divergentChild.weight
     }
 
-    private fun findProgramWithUnevenBalance(towerSupportPrograms: List<TowerSupportProgram>): TowerSupportProgram {
+    private fun findProgramWithUnevenlyBalancedChildren(towerSupportPrograms: List<TowerSupportProgram>): TowerSupportProgram {
         val programsThatSupportOthers = towerSupportPrograms
                 .filter { it.namesOfTowersItSupports != null }
         return programsThatSupportOthers
                 .find { parentProgram ->
-                    findAnyDivergenceInTotalWeights(parentProgram.towersItSupports)
+                    doChildrenProgramsHaveDivergenceInTotalWeights(parentProgram.towersItSupports)
                 }!!
         // we assume the input will give an unbalanced tower
     }
 
-    private fun findAnyDivergenceInTotalWeights(towersItSupports: List<TowerSupportProgram>): Boolean {
+    private fun doChildrenProgramsHaveDivergenceInTotalWeights(towersItSupports: List<TowerSupportProgram>): Boolean {
         val programsWithDistinctTotalWeights = towersItSupports.distinctBy { it.totalWeight }
         return programsWithDistinctTotalWeights.size > 1
     }
