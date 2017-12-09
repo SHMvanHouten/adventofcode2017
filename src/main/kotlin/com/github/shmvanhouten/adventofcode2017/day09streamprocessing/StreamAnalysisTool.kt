d578bfa5d4ab332088cf3f99569db0aa6574ccd0
SvanHouten <svanhouten@gmail.com>
2017-12-09 17:57:33 +0100

    found the solution to part 2

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day09streamprocessing/StreamAnalysisTool.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day09streamprocessing/StreamAnalysisTool.kt
@@ -2,15 +2,17 @@ package com.github.shmvanhouten.adventofcode2017.day09streamprocessing
 
 class StreamAnalysisTool {
 
-    fun findGroupValueInStream(stream: String): Int {
+    fun deleteGarbageAndFindGroupValue(stream: String): Pair<Int, Int> {
         var totalGroupValue = 0
         var currentGroupValue = 0
         var unAnalyzedStream = stream
+        var totalGarbageSize = 0
 
 
-        while (unAnalyzedStream.isNotEmpty()){
+        while (unAnalyzedStream.isNotEmpty()) {
             val firstRelevantCharacter = unAnalyzedStream[0]
-            when(firstRelevantCharacter){
+            when (firstRelevantCharacter) {
+
                 ',' -> {
                     unAnalyzedStream = unAnalyzedStream.substring(1)
                 }
@@ -21,22 +23,64 @@ class StreamAnalysisTool {
                 '}' -> {
                     totalGroupValue += currentGroupValue
                     currentGroupValue--
-                    unAnalyzedStream = unAnalyzedStream.substring( 1)
+                    unAnalyzedStream = unAnalyzedStream.substring(1)
                 }
                 '<' -> {
-                    unAnalyzedStream = deleteGarbageFromStream(unAnalyzedStream)
+                    val streamAfterGarbageDeletion = deleteGarbageFromStream(unAnalyzedStream)
+                    val garbage = unAnalyzedStream.substring(1, unAnalyzedStream.length - (streamAfterGarbageDeletion.length) - 1)
+
+                    totalGarbageSize += analyzeGarbage(garbage)
+
+                    unAnalyzedStream = streamAfterGarbageDeletion
                 }
             }
         }
 
-        return totalGroupValue
+        return Pair(totalGroupValue, totalGarbageSize)
+    }
+
+    private fun analyzeGarbage(garbage: String): Int {
+
+        var remainingGarbage = garbage
+        var amountOfDeletedChars = 0
+        var indexOfEscapeCharacter = remainingGarbage.indexOf('!')
+
+        while (indexOfEscapeCharacter != -1) {
+            amountOfDeletedChars += indexOfEscapeCharacter
+            remainingGarbage = removeEscapeCharactersFromGarbage(remainingGarbage.substring(indexOfEscapeCharacter))
+            indexOfEscapeCharacter = remainingGarbage.indexOf('!')
+        }
+
+        return amountOfDeletedChars + remainingGarbage.length
+    }
+
+    private fun removeEscapeCharactersFromGarbage(remainingGarbageFromEscapeChar: String): String {
+        var shouldDeleteNextChar = true
+        var index = 1
+        var charAtIndex = remainingGarbageFromEscapeChar[index]
+
+        while (charAtIndex == '!') {
+            shouldDeleteNextChar = !shouldDeleteNextChar
+            index++
+            if (index == remainingGarbageFromEscapeChar.length){
+                return ""
+            }
+
+            charAtIndex = remainingGarbageFromEscapeChar[index]
+        }
+
+        return if (shouldDeleteNextChar) {
+            remainingGarbageFromEscapeChar.substring(index + 1)
+        } else {
+            remainingGarbageFromEscapeChar.substring(index)
+        }
     }
 
     private tailrec fun deleteGarbageFromStream(unAnalyzedStream: String): String {
         val regex = Regex("""^(.*?)>""")
         val charactersUpToAndIncludingGarbageEnd = regex.find(unAnalyzedStream)!!
         val streamAfterEndIndicator = unAnalyzedStream.substring(charactersUpToAndIncludingGarbageEnd.range.last + 1)
-        if(isEndIndicatorValid(charactersUpToAndIncludingGarbageEnd.value)){
+        if (isEndIndicatorValid(charactersUpToAndIncludingGarbageEnd.value)) {
             return streamAfterEndIndicator
         }
         return deleteGarbageFromStream(streamAfterEndIndicator)
@@ -46,19 +90,10 @@ class StreamAnalysisTool {
         val charactersReversed = charactersUpToAndIncludingGarbageEnd.reversed().substring(1)
         var isValidIndicator = true
         charactersReversed.forEach {
-            if(it == '!') isValidIndicator = !isValidIndicator
+            if (it == '!') isValidIndicator = !isValidIndicator
             else return isValidIndicator
         }
         return isValidIndicator
     }
-
-}
-
-fun main(args: Array<String>) {
-    val regex = Regex("""^(.*?)>""")
-    val bla = "324324324324!!!>"
-    val find = regex.find(bla)!!
-    println(find.value)
-    println(find.range)
 
 }
--- src/test/kotlin/com/github/shmvanhouten/adventofcode2017/day09streamprocessing/StreamAnalysisToolTest.kt
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode2017/day09streamprocessing/StreamAnalysisToolTest.kt
@@ -2,7 +2,6 @@ package com.github.shmvanhouten.adventofcode2017.day09streamprocessing
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.junit.Ignore
 import org.junit.Test
 
 class StreamAnalysisToolTest {
@@ -10,83 +9,98 @@ class StreamAnalysisToolTest {
     @Test
     fun `it should find 1 group with a score of 1`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{}").first, equalTo(1))
     }
 
     @Test
     fun `it should find 2 groups with a score of 2`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{},{}"), equalTo(2))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{},{}").first, equalTo(2))
     }
 
     @Test
     fun `it should find 2 groups with a score of 3`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{{}}"), equalTo(3))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{{}}").first, equalTo(3))
     }
 
     @Test
     fun `it should find 3 groups with a score of 4`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{{}},{}"), equalTo(4))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{{}},{}").first, equalTo(4))
     }
 
     @Test
     fun `it should find 1 group with a score of 1 because the rest is garbage`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{<{}},{>}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<{}},{>}").first, equalTo(1))
     }
 
     @Test
     fun `it should find 1 group with a score of 1 because the rest is garbage with an escaped garbage end indicator`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{<{}!>}{>}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<{}!>}{>}").first, equalTo(1))
     }
 
     @Test
     fun `it should find 1 group with a score of 1 because the rest is garbage with a triple escaped garbage end indicator`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{<{}!!!>},{>}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<{}!!!>},{>}").first, equalTo(1))
     }
 
     @Test
     fun `it should find 1 group with a score of 1 because the rest is garbage with a double escaped garbage end indicator`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{<{}!!!>},{!!>}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<{}!!!>},{!!>}").first, equalTo(1))
     }
 
     @Test
     fun `it should give a score of 6`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{{{}}}"), equalTo(6))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{{{}}}").first, equalTo(6))
     }
 
     @Test
     fun `it should give a score of 5`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{{},{}}"), equalTo(5))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{{},{}}").first, equalTo(5))
     }
 
     @Test
     fun `it should give a score of 16`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{{{},{},{{}}}}"), equalTo(16))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{{{},{},{{}}}}").first, equalTo(16))
     }
 
     @Test
     fun `it should give a score of 1`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream("{<a>,<a>,<a>,<a>}"), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<a>,<a>,<a>,<a>}").first, equalTo(1))
     }
-
-
 
     @Test
     fun `it should solve the challenge input`() {
         val analysisTool = StreamAnalysisTool()
-        assertThat(analysisTool.findGroupValueInStream(day09Input), equalTo(1))
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue(day09Input).first, equalTo(20530))
     }
 
 
+    @Test
+    fun `it should find the amount of unescaped garbage characters to be 5`() {
+        val analysisTool = StreamAnalysisTool()
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<{}!!!>},{!!>}").second, equalTo(5))
+    }
+
+    @Test
+    fun `it should find the amount of unescaped garbage characters to be 4`() {
+        val analysisTool = StreamAnalysisTool()
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue("{<a>,<a>,<a>,<a>}").second, equalTo(4))
+    }
+
+    @Test
+    fun `it should find the amount of unescaped garbage characters in the challenge input to be 9978`() {
+        val analysisTool = StreamAnalysisTool()
+        assertThat(analysisTool.deleteGarbageAndFindGroupValue(day09Input).second, equalTo(9978))
+    }
 }
 
