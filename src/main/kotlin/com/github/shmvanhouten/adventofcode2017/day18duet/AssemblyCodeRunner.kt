30dac2daa575fd0464627ff813a896924c23f3d8
SvanHouten <svanhouten@gmail.com>
2017-12-19 09:14:16 +0100

    BSR: some method extractions for readability

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day18duet/AssemblyCodeRunner.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day18duet/AssemblyCodeRunner.kt
@@ -5,41 +5,23 @@ import com.github.shmvanhouten.adventofcode2017.day18duet.InstructionType.*
 class AssemblyCodeRunner {
     fun recoverFrequency(assemblyCode: List<AssemblyInstruction>): Long? {
 
-        var lastFrequency = 0L
         val registers = mutableMapOf<String, Long>()
 
+        var lastFrequency = 0L
         var index = 0L
 
         while (index < assemblyCode.size) {
             val instruction = assemblyCode[index.toInt()]
 
-            val firstValue = instruction.firstValue
-            val secondValue = instruction.secondValue
             when (instruction.instructionType) {
-                SOUND -> {
-                    lastFrequency = registers.getValue(firstValue as String)
-                }
-                SET -> {
-                    if(secondValue is Long) {
-                        registers.put(firstValue as String, secondValue)
-                    } else {
-                        registers.put(firstValue as String, registers.getOrPut(secondValue as String, {0}))
-                    }
-                }
+                SOUND -> lastFrequency = registers.getValue(instruction.firstValue as String)
+                SET -> performSetInstruction(registers, instruction)
                 ADD -> performAddInstruction(registers, instruction)
                 MULTIPLY -> performMultiplyInstruction(registers, instruction)
                 MODULO -> performModuloInstruction(registers, instruction)
-                // firstValue for recover is always a register
-                RECOVER -> if (registers.getValue(firstValue as String) != 0L) return lastFrequency
-                JUMP -> {
-                    val checkInt = firstValue as? Long ?: registers.getValue(firstValue as String)
-                    if(checkInt > 0L) {
-                        when (secondValue) {
-                            is Long -> index += secondValue - 1
-                            is String -> index += registers.getValue(secondValue) - 1
-                        }
-                    }
-                }
+            // firstValue for recover is always a register
+                RECOVER -> if (registers.getValue(instruction.firstValue as String) != 0L) return lastFrequency
+                JUMP -> index = performJumpInstruction(registers, instruction, index)
             }
             index++
         }
@@ -47,17 +29,39 @@ class AssemblyCodeRunner {
         return null
     }
 
+    private fun performSetInstruction(registers: MutableMap<String, Long>, instruction: AssemblyInstruction) {
+        val firstValue = instruction.firstValue as String
+        val secondValue = instruction.secondValue
+        if (secondValue is Long) {
+            registers.put(firstValue , secondValue)
+        } else {
+            registers.put(firstValue, registers.getOrPut(secondValue as String, { 0 }))
+        }
+    }
+
+    private fun performJumpInstruction(registers: MutableMap<String, Long>,instruction: AssemblyInstruction, index: Long): Long {
+        val checkLong = instruction.firstValue as? Long ?: registers.getValue(instruction.firstValue as String)
+        val secondValue = instruction.secondValue
+        if (checkLong > 0L) {
+            when (secondValue) {
+                is Long -> return index + secondValue - 1
+                is String -> return index + registers.getValue(secondValue) - 1
+            }
+        }
+        return index
+    }
+
     private fun performAddInstruction(registers: MutableMap<String, Long>, instruction: AssemblyInstruction) {
-        performOperation(instruction, registers, {first, second -> first + second})
+        performOperation(instruction, registers, { first, second -> first + second })
     }
 
 
     private fun performMultiplyInstruction(registers: MutableMap<String, Long>, instruction: AssemblyInstruction) {
-        performOperation(instruction, registers, {first, second -> first * second})
+        performOperation(instruction, registers, { first, second -> first * second })
     }
 
     private fun performModuloInstruction(registers: MutableMap<String, Long>, instruction: AssemblyInstruction) {
-        performOperation(instruction, registers, {first, second -> first % second})
+        performOperation(instruction, registers, { first, second -> first % second })
     }
 
 
@@ -65,8 +69,8 @@ class AssemblyCodeRunner {
         val register = instruction.firstValue as String
         val valueToPerformOperationWith = instruction.secondValue
         when (valueToPerformOperationWith) {
-            is String -> registers.put(register, operation(registers.getOrPut(register, {0L}), registers.getValue(valueToPerformOperationWith)))
-            is Long -> registers.put(register, operation(registers.getOrPut(register, {0L}), valueToPerformOperationWith))
+            is String -> registers.put(register, operation(registers.getOrPut(register, { 0L }), registers.getValue(valueToPerformOperationWith)))
+            is Long -> registers.put(register, operation(registers.getOrPut(register, { 0L }), valueToPerformOperationWith))
         }
     }
 
