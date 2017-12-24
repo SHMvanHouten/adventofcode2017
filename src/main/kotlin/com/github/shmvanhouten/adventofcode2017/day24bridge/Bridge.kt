f880330c71a5867c801abadbb23ec0bd57cf0c94
SjhmvanHouten <svanhouten@gmail.com>
2017-12-24 20:06:43 +0100

    made the strength field in Bridge lazy, making the length field lazy slowed things down unfortunately

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day24bridge/Bridge.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2017/day24bridge/Bridge.kt
@@ -2,7 +2,9 @@ package com.github.shmvanhouten.adventofcode2017.day24bridge
 
 data class Bridge(val components: List<BridgeComponent>, val openPort: Int) {
 
-    val strength: Int = components.sumBy { it.firstPort + it.secondPort }
+    val strength: Int by lazy {
+        components.sumBy { it.firstPort + it.secondPort }
+    }
 
     val length: Int = components.size
 
